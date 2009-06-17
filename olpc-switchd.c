6742ae56069164bc7c6354746a93a7e0765e93ba
Paul Fox <pgf@laptop.org>
2009-06-17 12:28:12 -0400

    add support for (optional) 4th input event device, which reports AC jack insertion/removal.  poll as before if it's not present.
    round the reported timestamps to the nearest second.

--- olpc-switchd.c
+++ olpc-switchd.c
@@ -74,6 +74,8 @@ int fifo_fd = -1;
 int pwr_fd = -1;
 int lid_fd = -1;
 int ebk_fd = -1;
+int acpwr_fd = -1;
+#define NUM_SWITCHES 4
 
 int maxfd = -1;  /* for select */
 
@@ -170,7 +172,7 @@ setup_input()
     unsigned char bit[bits2bytes(EV_MAX)];
     struct input_id id;
 
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < NUM_SWITCHES; i++) {
 
         snprintf(devname, sizeof(devname), "/dev/input/event%d", i);
         if ((dfd = open(devname, O_RDONLY)) < 0)
@@ -191,6 +193,23 @@ setup_input()
         if (i == 0)  pwr_fd = dfd;
         else if (i == 1)  lid_fd = dfd;
         else if (i == 2)  ebk_fd = dfd;
+        else if (i == 3) {
+            char name[32];
+            /* the AC power jack input device isn't available in
+             * all kernels.  if it's not here, we poll the /sys
+             * entry instead */
+            if(ioctl(dfd, EVIOCGNAME(sizeof(name)), name) < 0) {
+                report("failed ioctl EVIOCGBIT on %d", i);
+                close(dfd);
+                continue;
+            }
+            if (strncmp(name, "OLPC AC power", 13)) {
+                report("input%d is not OLPC AC power", i);
+                close(dfd);
+                continue;
+            }
+            acpwr_fd = dfd;
+        }
 
         maxfd = MAX(maxfd, dfd);
 
@@ -201,7 +220,7 @@ setup_input()
     else if (lid_fd == -1) report("didn't find lid");
     else if (ebk_fd == -1) report("didn't find ebook");
     else {
-        report("found all three switches");
+        report("found %d switches", acpwr_fd == -1 ? 3 : 4);
         ret = 0;
     }
 
@@ -268,6 +287,12 @@ send_event(char *evt, int seconds, char *extra)
 
 }
 
+long
+round_secs(struct input_event *ev)
+{
+    return ev->time.tv_sec + ((ev->time.tv_usec > 500000) ? 1 : 0);
+}
+
 void
 power_button_event()
 {
@@ -281,7 +306,7 @@ power_button_event()
         ev->type, ev->code, ev->value);
 
     if (ev->type == EV_KEY && ev->code == KEY_POWER && ev->value == 1)
-        send_event("powerbutton", ev->time.tv_sec, 0);
+        send_event("powerbutton", round_secs(ev), 0);
 
 
 }
@@ -298,11 +323,11 @@ lid_event()
         ev->time.tv_sec, ev->time.tv_usec,
         ev->type, ev->code, ev->value);
 
-    if (ev->type == EV_SW && ev->code == 0) {
+    if (ev->type == EV_SW && ev->code == SW_LID) {
         if (ev->value)
-            send_event("lidclose", ev->time.tv_sec, 0);
+            send_event("lidclose", round_secs(ev), 0);
         else
-            send_event("lidopen", ev->time.tv_sec, 0);
+            send_event("lidopen", round_secs(ev), 0);
     }
 
 }
@@ -319,11 +344,34 @@ ebook_event()
         ev->time.tv_sec, ev->time.tv_usec,
         ev->type, ev->code, ev->value);
 
-    if (ev->type == EV_SW && ev->code == 1) {
+    if (ev->type == EV_SW && ev->code == SW_TABLET_MODE) {
         if (ev->value)
-            send_event("ebookclose", ev->time.tv_sec, 0);
+            send_event("ebookclose", round_secs(ev), 0);
         else
-            send_event("ebookopen", ev->time.tv_sec, 0);
+            send_event("ebookopen", round_secs(ev), 0);
+    }
+}
+
+void
+acpwr_event()
+{
+    struct input_event ev[1];
+
+    if (read(acpwr_fd, ev, sizeof(ev)) != sizeof(ev))
+        die("bad read from AC power jack");
+
+    dbg(3, "acpwr: ev sec %d usec %d type %d code %d value %d",
+        ev->time.tv_sec, ev->time.tv_usec,
+        ev->type, ev->code, ev->value);
+
+#ifndef SW_DOCK
+#define SW_DOCK 5
+#endif
+    if (ev->type == EV_SW && ev->code == SW_DOCK) {
+        if (ev->value)
+            send_event("ac-online", round_secs(ev), 0);
+        else
+            send_event("ac-offline", round_secs(ev), 0);
     }
 }
 
@@ -336,24 +384,26 @@ poll_power_sources(void)
     static int was_online = -1;
     static int was_capacity = -1;
 
-    fd = open("/sys/class/power_supply/olpc-ac/online", O_RDONLY);
-    if (fd < 0)
-        return;
+    if (acpwr_fd < 0) {
+        fd = open("/sys/class/power_supply/olpc-ac/online", O_RDONLY);
+        if (fd < 0)
+            return;
 
-    r = read(fd, buf, 1);
-    if (r != 1 || (buf[0] != '0' && buf[0] != '1')) {
+        r = read(fd, buf, 1);
+        if (r != 1 || (buf[0] != '0' && buf[0] != '1')) {
+            close(fd);
+            return;
+        }
+
+        online = buf[0] - '0';
+
+        if (was_online != online) {
+            send_event(online ? "ac-online" : "ac-offline", time(0), 0);
+            was_online = online;
+        }
+
         close(fd);
-        return;
     }
-
-    online = buf[0] - '0';
-
-    if (was_online != online) {
-        send_event(online ? "ac-online" : "ac-offline", time(0), 0);
-        was_online = online;
-    }
-
-    close(fd);
 
     fd = open("/sys/class/power_supply/olpc-battery/capacity", O_RDONLY);
     if (fd < 0)
@@ -392,11 +442,15 @@ data_loop(void)
         FD_SET(pwr_fd, &inputs);
         FD_SET(lid_fd, &inputs);
         FD_SET(ebk_fd, &inputs);
+        if (acpwr_fd >= 0)
+            FD_SET(acpwr_fd, &inputs);
 
         FD_ZERO(&errors);
         FD_SET(pwr_fd, &errors);
         FD_SET(lid_fd, &errors);
         FD_SET(ebk_fd, &errors);
+        if (acpwr_fd >= 0)
+            FD_SET(acpwr_fd, &errors);
 
         if (pollinterval) {
             tv.tv_sec = pollinterval;
@@ -420,6 +474,8 @@ data_loop(void)
                 die("select reports error on lid switch");
             if (FD_ISSET(ebk_fd, &errors))
                 die("select reports error on ebook switch");
+            if (acpwr_fd >= 0 && FD_ISSET(acpwr_fd, &errors))
+                die("select reports error on ac power jack");
 
             if (FD_ISSET(pwr_fd, &inputs))
                 power_button_event();
@@ -427,6 +483,8 @@ data_loop(void)
                 lid_event();
             if (FD_ISSET(ebk_fd, &inputs))
                 ebook_event();
+            if (acpwr_fd >= 0 && FD_ISSET(acpwr_fd, &inputs))
+                acpwr_event();
         
             if (sysactive_path)
                 indicate_activity();
