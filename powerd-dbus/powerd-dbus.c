003aa51f4ec890b17fa07b7cabf86860a15cb3b9
Daniel Drake <dsd@laptop.org>
2011-08-05 20:03:49 +0100

    powerd-dbus: convert to full-time daemon
    No longer shuts down after 30 seconds.

--- powerd-dbus/powerd-dbus.c
+++ powerd-dbus/powerd-dbus.c
@@ -29,23 +29,6 @@ void powerd_send_event(const char *event)
 	close(fd);
 }
 
-/* maximum runtime of 30 seconds, unless we're busy doing something at
- * that point. dbus will launch us again if needed. */
-static gboolean exit_timeout(gpointer data)
-{
-	GError *error = NULL;
-	guint32 ret;
-
-	if (g_main_context_pending(NULL))
-		return TRUE;
-
-	org_freedesktop_DBus_release_name(driver_proxy, OHM_DBUS_SERVICE, &ret,
-		&error);
-	g_message("exit after timeout");
-	g_main_loop_quit(mainloop);
-	return FALSE;
-}
-
 int main(void)
 {
 	GError *error = NULL;
@@ -53,8 +36,6 @@ int main(void)
 
 	g_type_init();
 	mainloop = g_main_loop_new(NULL, FALSE);
-
-	g_timeout_add_seconds(30, exit_timeout, NULL);
 
 	dbus_conn = dbus_g_bus_get(DBUS_BUS_SYSTEM, &error);
 	if (dbus_conn == NULL)
